6281388d7d929ba4a3b35cb1a9f55e2403c3ec09
권인 <in24041210@gmail.com>
2025-05-22 23:56:42 +0900

    temp: 회원정보 수정 기능 구현 중

--- final-pjt-front/final-pjt/src/main.js
+++ final-pjt-front/final-pjt/src/main.js
@@ -1,14 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useAuthStore } from './stores/auth'
+import axios from 'axios'
+
+import vuetify from './plugins/vuetify'
 
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
+const auth = useAuthStore(pinia)
+
+if (auth.token) {
+    axios.defaults.headers.common['Authorization'] = `Token ${auth.token}`
+  }
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
+
--- final-pjt-front/final-pjt/src/router/index.js
+++ final-pjt-front/final-pjt/src/router/index.js
@@ -1,22 +1,29 @@
+import LoginView from '@/views/LoginView.vue'
+import ProfileEditView from '@/views/ProfileEditView.vue'
+import SignupView from '@/views/SignupView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/signup',
+      name: 'signup',
+      component: SignupView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
+    // src/router/index.js
+    {
+      path: '/profile/edit',
+      name: 'ProfileEdit',
+      component: ProfileEditView,
+      meta: { requiresAuth: true }
+    },
+
   ],
 })
 
