27eecbceda5f19f2abfa96cf68f3b5f0e7caf8f8
김혜정 <hjeong91108@gmail.com>
2025-05-23 09:49:06 +0900

    feat: 회원 수정 요청 처리

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,3 +1,5 @@
+from allauth.account.utils import setup_user_email
+from allauth.account.models import EmailAddress
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from dj_rest_auth.serializers import UserDetailsSerializer
 
@@ -5,8 +7,13 @@ from rest_framework import serializers
 from allauth.account.adapter import get_adapter
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.conf import settings
 
 from django.contrib.auth import get_user_model
+
+# 이메일 필드 관련
+from allauth.account.models import EmailAddress
+from allauth.account.utils import setup_user_email
 
 
 class CustomRegisterSerializer(RegisterSerializer):
@@ -33,7 +40,8 @@ class CustomRegisterSerializer(RegisterSerializer):
 
         if "password1" in self.cleaned_data:
             try:
-                adapter.clean_password(self.cleaned_data["password1"], user=user)
+                adapter.clean_password(
+                    self.cleaned_data["password1"], user=user)
             except DjangoValidationError as exc:
                 raise serializers.ValidationError(
                     detail=serializers.as_serializer_error(exc)
@@ -45,15 +53,32 @@ class CustomRegisterSerializer(RegisterSerializer):
 class CustomUserDetailsSerializer(UserDetailsSerializer):
     age = serializers.IntegerField(required=True)
     nickname = serializers.CharField(max_length=150, required=True)
-    username = serializers.CharField(read_only=True)  # username은 읽기 전용
+    username = serializers.CharField(read_only=True)
     email = serializers.EmailField(required=True)
 
     class Meta(UserDetailsSerializer.Meta):
         model = get_user_model()
-        fields = UserDetailsSerializer.Meta.fields + ("email", "age", "nickname")
+        fields = UserDetailsSerializer.Meta.fields + \
+            ("email", "age", "nickname")
         read_only_fields = ("username",)
 
     def update(self, instance, validated_data):
-        instance.email = validated_data.get("email", instance.email)
+        new_email = validated_data.get("email")
+        if new_email and new_email != instance.email:
+            # 방법 A) allauth helper 사용
+            setup_user_email(
+                self.context["request"],
+                instance,
+                new_email,
+                confirm=False  # 이메일 확인 이메일을 보내지 않도록
+            )
+            # 또는 방법 B) 직접 업데이트
+            # instance.email = new_email
+            # instance.save()
+            # EmailAddress.objects.filter(user=instance, primary=True) \
+            #     .update(email=new_email)
+        # 2) 나머지 필드
+        instance.age = validated_data.get("age", instance.age)
+        instance.nickname = validated_data.get("nickname", instance.nickname)
         instance.save()
         return instance
--- final-pjt-back/articles/views.py
+++ final-pjt-back/articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article, Comment
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def article_list(request):
+    if request.method == "GET":
+        pass
