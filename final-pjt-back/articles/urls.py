01d4cb8b176a3d7d62d9788b7dd64d018cb1c3d5
김혜정 <hjeong91108@gmail.com>
2025-05-23 12:43:31 +0900

    feat: comment 기능

--- final-pjt-back/articles/urls.py
+++ final-pjt-back/articles/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.article_list),
     path('<int:article_pk>/', views.article_detail),
-    # path('comment/', views.comment_list),
-    # path('comment/<int:comment_pk>/', views.comment_detail),
+    path('<int:article_pk>/comment/', views.comment_list),
+    path('<int:article_pk>/comment/<int:comment_pk>/', views.comment_detail),
 ]
