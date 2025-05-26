66886b81b883700ad117d2ed64253e3321c4c887
김혜정 <hjeong91108@gmail.com>
2025-05-26 17:29:57 +0900

    temp: db 구조 변경 준비

--- final-pjt-back/financial_products/urls.py
+++ final-pjt-back/financial_products/urls.py
@@ -1,10 +1,19 @@
+from .views import deposit_companies, saving_companies
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from . import views
-
+from .views import DepositProductViewSet, SavingProductViewSet
 
 router = DefaultRouter()
-router.register(r'deposits', views.DepositProductViewSet, basename='deposit')
-router.register(r'savings',  views.SavingProductViewSet,  basename='saving')
+router.register(r'deposits',
+                DepositProductViewSet, basename='deposit')
+router.register(r'savings', SavingProductViewSet, basename='saving')
 
 urlpatterns = router.urls
+
+
+urlpatterns += [
+    path("products/deposits/companies/",
+         deposit_companies, name="deposit-companies"),
+    path("products/savings/companies/",
+         saving_companies, name="saving-companies"),
+]
