5ad8a940a325a533d23e84e267cc2afb8a2f75aa
김혜정 <hjeong91108@gmail.com>
2025-05-26 17:03:33 +0900

    fix: 금융 상품 정보 일부 정보만 제공/페이지네이션

--- final-pjt-back/financial_products/views.py
+++ final-pjt-back/financial_products/views.py
@@ -1,51 +1,35 @@
-from datetime import datetime
 
-from .models import Stock, DepositProduct
+from .models import DepositProduct
 from .models import SavingProduct
 from django.shortcuts import render
-from django.conf import settings
 
-from rest_framework.response import Response
-from rest_framework.decorators import (
-    api_view,
-    permission_classes,
-    authentication_classes,
-)
-from rest_framework import status
-from rest_framework.permissions import (
-    IsAuthenticated,
-    AllowAny,
-    IsAuthenticatedOrReadOnly,
-)
-from rest_framework.decorators import api_view
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.viewsets import ReadOnlyModelViewSet
-
-from rest_framework.decorators import action
-from .services import fetch_and_upsert_saving
-from .services import fetch_and_upsert_deposit
-from django_celery_beat.models import PeriodicTask
-
-# views.py
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import DepositProduct, SavingProduct
 from .serializers import (
     DepositProductReadSerializer,
     SavingProductReadSerializer,
 )
+from .pagination import CustomPageNumberPagination
+
 
 class DepositProductViewSet(ReadOnlyModelViewSet):
     """
-    GET  /api/deposits/        → 예금 상품 리스트
-    GET  /api/deposits/{pk}/   → 단건 조회
+    GET  /products/deposits/        → 예금 상품 리스트
+    GET  /products/deposits/{pk}/   → 단건 조회
     """
-    queryset = DepositProduct.objects.all().order_by('-dcls_strt_day')
+    queryset = DepositProduct.objects.prefetch_related(
+        "depositproductoptions").order_by('-dcls_strt_day')
     serializer_class = DepositProductReadSerializer
+    pagination_class = CustomPageNumberPagination
+
 
 class SavingProductViewSet(ReadOnlyModelViewSet):
     """
-    GET  /api/savings/         → 적금 상품 리스트
-    GET  /api/savings/{pk}/    → 단건 조회
+    GET  /products/savings/         → 적금 상품 리스트
+    GET  /products/savings/{pk}/    → 단건 조회
     """
-    queryset = SavingProduct.objects.all().order_by('-dcls_strt_day')
+    queryset = SavingProduct.objects.prefetch_related(
+        "savingproductoptions").order_by('-dcls_strt_day')
     serializer_class = SavingProductReadSerializer
+    pagination_class = CustomPageNumberPagination
